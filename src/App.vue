90bae49683963872b29419f1f6ae6b963d80d255
Yura Komarov <yurajun@mail.ru>
2021-06-01 09:25:45 +0300

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,26 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+#app
+    img(src="./assets/logo.png", alt="Vue logo")
+    HelloWorld(msg="Welcome to Your Vue.js App")
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+    name: 'App',
+    components: {
+        HelloWorld,
+    },
+}
 </script>
 
 <style lang="stylus">
 #app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+    margin-top 60px
+    font-family Avenir, Helvetica, Arial, sans-serif
+    text-align center
+    color #2c3e50
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
 </style>
